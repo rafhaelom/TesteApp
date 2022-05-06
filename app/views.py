af7f361c55cd402b2756f2c51f920fafead08278
rafhael <rafhael.martins@iesb.edu.br>
2022-05-06 20:06:34 -0300

    Segundo commit script em desenvolvimento

--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
 from app import app
+from datetime import datetime
 
 @app.route('/')
 def home():
    return "hello world!"
+
+@app.route('/hora')
+def hora():
+   return f"{datetime.now()}"
+
+@app.route('/soma')
+def soma():
+   num1 = request.form['num1']
+   num2 = request.form['num2']
+   soma = int(num1 + num2)
+   return f"{soma}"
+
